156013b120a5d4c8b161782457647d1bd9abfb46
Kyle Bashour <kylebshr@me.com>
2018-08-14 22:25:12 -0700

    Add missing framework stuff

---
+++ Sources/Mammut/Mammut.h
@@ -0,0 +1,18 @@
+//
+//  Mammut.h
+//  Mammut
+//
+//  Created by Kyle Bashour on 8/14/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mammut.
+FOUNDATION_EXPORT double MammutVersionNumber;
+
+//! Project version string for Mammut.
+FOUNDATION_EXPORT const unsigned char MammutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mammut/PublicHeader.h>
+
+
