9cc7cab0861f39934f72b31f57355a381dcf6cac
Kyle Bashour <kylebshr@me.com>
2018-08-19 16:28:15 -0700

    WIP on networking

---
+++ Sources/Mammut/Sources/Network.swift
@@ -0,0 +1,93 @@
+import Foundation
+
+public struct App: Codable {
+    var clientName: String
+    var redirectUris: String
+    var scopes: String
+    var website: URL?
+    
+    public init(clientName: String, redirectUris: String = "urn:ietf:wg:oauth:2.0:oob",
+         scopes: String = "read write follow", website: URL? = nil) {
+        self.clientName = clientName
+        self.redirectUris = redirectUris
+        self.scopes = scopes
+        self.website = website
+    }
+}
+
+public struct Client: Codable {
+    var id: String
+    var clientId: String
+    var clientSecret: String
+}
+
+public enum Result<T: Codable, U: Error> {
+    case success(T)
+    case error(U)
+}
+
+public struct Request<Model: Codable> {
+    public enum Method: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    let model: Model
+    let method: Method
+    
+    public init(model: Model, method: Method) {
+        self.model = model
+        self.method = method
+    }
+}
+
+public struct NetworkError: Error {
+
+}
+
+extension URL: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StaticString) {
+        guard let url = URL(string: String(describing: value)) else {
+            fatalError("Invalid URL string literal: \(value)")
+        }
+        self = url
+    }
+}
+
+public struct Network {
+    public typealias ClientCompletion<T: Codable> = (Result<T, NetworkError>) -> Void
+    
+    private let decoder = JSONDecoder()
+    private let encoder = JSONEncoder()
+    
+    private let baseURL: URL
+    
+    public init(baseURL: URL) {
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        self.baseURL = baseURL
+    }
+    
+    public func perform<T: Codable, U: Codable>(request: Request<T>, endpoint: String, completion: @escaping ClientCompletion<U>) {
+        guard let data = try? encoder.encode(request.model) else {
+            return completion(.error(NetworkError()))
+        }
+        
+        let url = baseURL.appendingPathComponent(endpoint)
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = request.method.rawValue
+        urlRequest.httpBody = data
+        urlRequest.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let data = data, let model = try? self.decoder.decode(U.self, from: data) {
+                completion(.success(model))
+            } else {
+                completion(.error(NetworkError()))
+            }
+        }
+        
+        task.resume()
+    }
+}
--- Sources/Wooly/Sources/Application/AppDelegate.swift
+++ Sources/Wooly/Sources/Application/AppDelegate.swift
@@ -7,7 +7,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Properties
 
     private let window = UIWindow()
-
+    private let network = Network(baseURL: "https://mastodon.social/api/v1")
+    
     // MARK: - UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -17,6 +18,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.tintColor = .white
         window.rootViewController = RootViewController()
         window.makeKeyAndVisible()
+        
+        let app = App(clientName: "wooly-ios")
+        let request = Request(model: app, method: .post)
+
+        network.perform(request: request, endpoint: "apps") { (result: Result<Client, NetworkError>) in
+            switch result {
+            case .success(let client):
+                print(client)
+            case .error(let error):
+                print(error)
+            }
+        }
+        
         return true
     }
 }
