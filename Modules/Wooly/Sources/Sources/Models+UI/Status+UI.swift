fd4f4f1cc337ba589137800e20c97592a0fa2e53
Kyle Bashour <kylebshr@me.com>
2018-08-26 22:08:23 -0700

    Remove texture and html parsing

--- Modules/Wooly/Sources/Sources/Models+UI/Status+UI.swift
+++ Modules/Wooly/Sources/Sources/Models+UI/Status+UI.swift
@@ -2,19 +2,6 @@ import Mammut
 
 extension Status {
     var strippedContent: String? {
-        guard let data = content.data(using: .utf8) else {
-            return nil
-        }
-
-        let options: [NSAttributedString.DocumentReadingOptionKey : Any] = [
-            .documentType: NSAttributedString.DocumentType.html,
-            .characterEncoding: String.Encoding.utf8.rawValue
-        ]
-
-        guard let htmlString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
-            return nil
-        }
-
-        return String(htmlString.string.dropLast())
+        return content
     }
 }
